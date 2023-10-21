082a1045ee059b482af1c0ed202ea08770e5a7d9
Subha <fabihafairuzz@gmail.com>
2023-10-21 01:52:18 -0600

    Added comments

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-# Game board created
-# Display the board
-# Keep track of scores
-    # Check row
-    # Check column
-    # Check diagonal
-    # Check if its a tie
-# Alternate turns between players (option to play with another player or the computer)
+# Tic Tac Toe Game in Python
 
-# Creating a board
+# Defining the global variables
 board = ["_","_","_",
          "_","_","_",
          "_","_","_"]
@@ -17,39 +10,42 @@ gameContinues = True
 winner = None
 
 
-
+# Function to display the board
 def displayBoard():
     print("Current Game Board \n")
     print(board[0] + " | " + board[1] + " | " + board[2] + "\n" +
           board[3] + " | " + board[4] + " | " + board[5] + "\n" +
           board[6] + " | " + board[7] + " | " + board[8] + "\n")
     
-    
+ # Function for playing the game    
 def playGame():
     
     # Display the initial board
     displayBoard()
-    
+    # Looping until the game stops
     while gameContinues:
         
+        # Changing between the player turns
         handleTurns(currentPlayer)
-        
+        #Check if the game is over or not after every turn
         checkGameOver()
         if winner:
             break
         changePlayer()
-        
+    # If game over print the results    
     if winner == "X" or winner == "O":
         print("Player " + winner + " won!")
     else:
         print("Its a tie!")
         
         
-    
+# Function to change between the turns of the player    
 def handleTurns(currentPlayer):
     while True:
+        # Getting the player input
         position = input("Player " + currentPlayer + "\n" +
                          "Choose a position from 1-9: " + "\n")
+        # Checking if the input is valid and within the range of the board
         if position.isdigit() and 1 <= int(position) <= 9:
             position = int(position) - 1
 
@@ -60,53 +56,45 @@ def handleTurns(currentPlayer):
 
     # Update the board
     board[position] = currentPlayer
+    # Display the board
     displayBoard()
-    
+ 
+# Functionto check if the game is over    
 def checkGameOver():
     
+    # Call function to check if there is a win
     checkWin()
+    # Otherwise call the function to check for a tie
     checkTie()
     
-    #set gameContinue to false
-    
+# Function to check the win
 def checkWin():
-    
-    checkRows()
-    checkColumns()
-    checkDiagonals()
-    
-    return
-
-def checkRows():
-    
     global winner
+    # Check rows
     for i in range(0, 7, 3):
         if board[i] == board[i + 1] == board[i + 2] != "_":
             winner = board[i]
             return
-        
-def checkColumns():
-    global winner
+
+    # Check columns
     for i in range(3):
         if board[i] == board[i + 3] == board[i + 6] != "_":
             winner = board[i]
             return
-        
-def checkDiagonals():
-    global winner
+
+    # Check diagonals
     if board[0] == board[4] == board[8] != "_":
         winner = board[0]
     elif board[2] == board[4] == board[6] != "_":
         winner = board[2]
-    
-    
-    
+
+# Function to check for a tie        
 def checkTie():
     global gameContinues
     if "_" not in board and not winner:
         gameContinues = False
     
-
+# Function to change the player symbol after every turn
 def changePlayer(): 
     global currentPlayer
     if currentPlayer == "X":
@@ -114,7 +102,7 @@ def changePlayer():
     else:
         currentPlayer = "X"
 
-  
+# Execute the game 
 playGame()   
      
      
