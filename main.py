abe22f45caeea00d6922fa57500f773caeb658c6
Subha <fabihafairuzz@gmail.com>
2023-10-26 02:19:38 -0600

    Implement GUI to play the game

--- main.py
+++ main.py
@@ -1,99 +1,57 @@
-# Tic Tac Toe Game in Python
+import tkinter as tk
+
+# Creating the main window
+root = tk.Tk()
+root.title("Tic Tac Toe")
+root.geometry("500x500")  # Adjusting the initial size of the window
 
 # Defining the global variables
-board = ["_","_","_",
-         "_","_","_",
-         "_","_","_"]
+board = [" "]*9
 
 currentPlayer = "X"
 gameContinues = True
 winner = None
 
-
-# Function to display the board
-def displayBoard():
-    print("Current Game Board \n")
-    print(board[0] + " | " + board[1] + " | " + board[2] + "\n" +
-          board[3] + " | " + board[4] + " | " + board[5] + "\n" +
-          board[6] + " | " + board[7] + " | " + board[8] + "\n")
-    
- # Function for playing the game    
-def playGame():
-    
-    # Display the initial board
-    displayBoard()
-    # Looping until the game stops
-    while gameContinues:
-        
-        # Changing between the player turns
-        handleTurns(currentPlayer)
-        #Check if the game is over or not after every turn
+# Function to handle the button click
+def on_button_click(i):
+    global currentPlayer, winner, gameContinues
+    if gameContinues and board[i] == " ":
+        board[i] = currentPlayer
+        buttons[i].config(text=currentPlayer)
         checkGameOver()
         if winner:
-            break
+            gameContinues = False
+            print(f"Player {winner} won!")
         changePlayer()
-    # If game over print the results    
-    if winner == "X" or winner == "O":
-        print("Player " + winner + " won!")
-    else:
-        print("Its a tie!")
-        
-        
-# Function to change between the turns of the player    
-def handleTurns(currentPlayer):
-    while True:
-        # Getting the player input
-        position = input("Player " + currentPlayer + "\n" +
-                         "Choose a position from 1-9: " + "\n")
-        # Checking if the input is valid and within the range of the board
-        if position.isdigit() and 1 <= int(position) <= 9:
-            position = int(position) - 1
 
-            if board[position] == "_":
-                break
-            else:
-                print("Position already taken. Choose another position.")
-
-    # Update the board
-    board[position] = currentPlayer
-    # Display the board
-    displayBoard()
- 
-# Functionto check if the game is over    
+# Function to check if the game is over
 def checkGameOver():
-    
-    # Call function to check if there is a win
     checkWin()
-    # Otherwise call the function to check for a tie
     checkTie()
-    
+
 # Function to check the win
 def checkWin():
     global winner
-    # Check rows
     for i in range(0, 7, 3):
-        if board[i] == board[i + 1] == board[i + 2] != "_":
+        if board[i] == board[i + 1] == board[i + 2] != " ":
             winner = board[i]
             return
-
-    # Check columns
     for i in range(3):
-        if board[i] == board[i + 3] == board[i + 6] != "_":
+        if board[i] == board[i + 3] == board[i + 6] != " ":
             winner = board[i]
             return
-
-    # Check diagonals
-    if board[0] == board[4] == board[8] != "_":
+    if board[0] == board[4] == board[8] != " ":
         winner = board[0]
-    elif board[2] == board[4] == board[6] != "_":
+    elif board[2] == board[4] == board[6] != " ":
         winner = board[2]
 
-# Function to check for a tie        
+# Function to check for a tie
 def checkTie():
     global gameContinues
-    if "_" not in board and not winner:
+    if " " not in board and not winner:
         gameContinues = False
-    
+        print("It's a tie!")
+
 # Function to change the player symbol after every turn
 def changePlayer(): 
     global currentPlayer
@@ -102,11 +60,42 @@ def changePlayer():
     else:
         currentPlayer = "X"
 
-# Execute the game 
-playGame()   
-     
-     
-     
-    
-    
-    
+# Function to start a new game
+def new_game():
+    global board, currentPlayer, gameContinues, winner
+    for i in range(9):
+        buttons[i].config(text=" ")
+    board = [" "]*9
+    currentPlayer = "X"
+    gameContinues = True
+    winner = None
+
+# Function to quit the game
+def quit_game():
+    root.quit()
+
+# Creating the buttons for the board
+buttons = []
+for i in range(9):
+    row = i // 3
+    col = i % 3
+    button = tk.Button(root, text=board[i], font=("Helvetica", 70), width=2, height=1, command=lambda i=i: on_button_click(i))
+    button.grid(row=row, column=col, sticky="nsew")  # Modifying the sticky parameter
+    buttons.append(button)
+
+# Configuring rows and columns to expand proportionally
+for i in range(3):
+    root.grid_rowconfigure(i, weight=1)
+    root.grid_columnconfigure(i, weight=1)
+
+# Creating a menu
+menu = tk.Menu(root)
+root.config(menu=menu)
+file_menu = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Menu", menu=file_menu)
+file_menu.add_command(label="New Game", command=new_game)
+file_menu.add_separator()
+file_menu.add_command(label="Quit", command=quit_game)
+
+# Running the main loop
+root.mainloop()
